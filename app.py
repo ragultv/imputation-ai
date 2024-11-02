883a64034d9e0ee372cc2b7020f9a8742c3e39a4
ragul <141637129+ragultv@users.noreply.github.com>
2024-11-02 16:54:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -350,5 +350,5 @@ def impute_data():
 def download_file(filename):
     return send_file(os.path.join(IMPUTED_FOLDER, filename), as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
